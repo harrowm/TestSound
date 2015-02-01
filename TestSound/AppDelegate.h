6b85190cc9f398e6889ea4ee2089d807bf23710a
Malcolm Harrow <malcolm.harrow@gmail.com>
2015-02-01 12:12:22 +0800

    Initial Commit

---
+++ TestSound/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSound
+//
+//  Created by Malcolm on 1/2/15.
+//  Copyright (c) 2015 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
